4fc78cf7723eedd4dfdd17245411eb4b386f67f6
TomMctomald <60165947+TomMctomald@users.noreply.github.com>
2020-02-04 19:13:38 -0600

    Add files via upload
    temp company, temporarily useless info

---
+++ Group-Assignment-1-master.zip_expanded/Group-Assignment-1-master/eclipse/src/ics372/assignment1/test/ShippingTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package ics372.assignment1.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author conor
+ *
+ */
+class ShippingTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Group-Assignment-1-master.zip_expanded/Group-Assignment-1-master/eclipse/src/module-info.java
@@ -0,0 +1,8 @@
+module GroupAssignment1 {
+	requires org.junit.jupiter.api;
+	requires json.simple;
+	requires java.desktop;
+	requires com.google.gson;
+	//exports ics372.assignment1.model;
+
+}
