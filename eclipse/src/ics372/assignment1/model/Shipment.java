28133522c3ea56e80d21a28c4a5b60310c31d928
nicole-moritz <hb6086ew@go.minnstate.edu>
2020-02-06 17:40:38 -0600

    warehouse

--- eclipse/src/ics372/assignment1/model/Shipment.java
+++ eclipse/src/ics372/assignment1/model/Shipment.java
@@ -1,6 +1,58 @@
 package ics372.assignment1.model;
 
 public class Shipment {
-	//comment
+	int warehouse_id;
+	String shipment_method;
+	String shipment_id;
+	double weight;
+	int receipt_date;
+	
+	public Shipment (int warehouse_id, String shipment_method, String shipment_id, double weight, int receipt_date) {
+		this.warehouse_id = warehouse_id;
+		this.shipment_method = shipment_method;
+		this.shipment_id = shipment_id;
+		this.weight = weight;
+		this.receipt_date = receipt_date;
+	}
+
+	public int getWarehouse_id() {
+		return warehouse_id;
+	}
+
+	public void setWarehouse_id (int warehouse_id) {
+		this.warehouse_id = warehouse_id;
+	}
+	
+	public String getShipment_method() {
+		return shipment_method;
+	}
+
+	public void setShipment_method(String shipment_method) {
+		this.shipment_method = shipment_method;
+	}
+
+	public String getShipment_id() {
+		return shipment_id;
+	}
+
+	public void setShipment_id(String shipment_id) {
+		this.shipment_id = shipment_id;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public int getReceipt_date() {
+		return receipt_date;
+	}
+
+	public void setReceipt_date(int receipt_date) {
+		this.receipt_date = receipt_date;
+	}
 
 }
--- eclipse/src/ics372/assignment1/model/Warehouse.java
+++ eclipse/src/ics372/assignment1/model/Warehouse.java
@@ -1,5 +1,53 @@
 package ics372.assignment1.model;
+import java.util.Arrays;
 
 public class Warehouse {
+	
+	static int warehouse_id;
+	private Shipment [] incoming_shipments;
+	int many_items = 0;
+
+	public Warehouse (int capacity, int warehouse_id) {
+		incoming_shipments = new Shipment [capacity];
+		this.warehouse_id = warehouse_id;
+	} 
+	
+	public int getWarehouse_id() {
+		return warehouse_id;
+	}
+
+	public void setWarehouse_id(int warehouse_id) {
+		this.warehouse_id = warehouse_id;
+	}
+	
+	//***CHANGE*** use some sort of compaeTo to see if shipment has enabled freight recepit 
+	public int compareTo (Shipment shipment) {
+		int output = -1;
+		if (this.getWarehouse_id() == shipment.getWarehouse_id())
+			output = 0;
+		if (this.getWarehouse_id() != shipment.getWarehouse_id())
+			output = 1;
+		return output;
+	}
+	
+	//method: add incoming shipment
+	
+	//how do I add only the shipment_id and weight???
+	
+	
+	//add newShipment if they have 1/4 shipping methods (is this a requirement)
+	//use if/else to check if incoming shipment has an enabled recipet
+	//associate the shipment with the specified warehouse_id
+	
+	public void adding_incoming_shipment (int warehouse_id, String shipment_method, String shipment_id, double weight, int receipt_date) {
+		Shipment newshipment = new Shipment (warehouse_id, shipment_method, shipment_id, weight, receipt_date);
+		
+		if ()
+		
+	}
+		
+	//method: enable freight receipt
+	
+	//method: end freight receipt
 
 }
