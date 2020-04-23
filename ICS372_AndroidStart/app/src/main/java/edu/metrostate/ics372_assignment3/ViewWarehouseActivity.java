531d9e7a75dfafb5a0a812f976a9a469549f4152
conorm185 <43192407+conorm185@users.noreply.github.com>
2020-04-22 22:09:44 -0500

    Added ability to view individual shipments to ViewWarehouseActivity

--- ICS372_AndroidStart/app/src/main/java/edu/metrostate/ics372_assignment3/ViewWarehouseActivity.java
+++ ICS372_AndroidStart/app/src/main/java/edu/metrostate/ics372_assignment3/ViewWarehouseActivity.java
@@ -4,25 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
 import edu.metrostate.ics372_assignment3.model.Company;
 import edu.metrostate.ics372_assignment3.model.Shipment;
 
-public class ViewWarehouseActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
+public class ViewWarehouseActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
 
     private Company company;
     private Button addShipmentButton, toggleActiveInactiveButton, toggleRecieptButton, editWarehouseButton;
+    private TextView warehouse_id, warehouse_name, shipment_warehouse_id,shipment_shipment_id, shipment_method, shipment_weight, shipment_receipt, shipment_departure;
     private ListView shipmentList;
     private HashMap<String, Shipment> warehouse_contents;
-    WarehouseApplication application;
+    private WarehouseApplication application;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +43,29 @@ public class ViewWarehouseActivity extends AppCompatActivity implements View.OnC
         toggleRecieptButton = (Button) findViewById(R.id.receiptButton);
         editWarehouseButton = (Button) findViewById(R.id.editWarehouseButton);
 
+        warehouse_id = (TextView) findViewById(R.id.warehouseIDText);
+        warehouse_name = (TextView) findViewById(R.id.warehouseNameText);
+        shipment_warehouse_id= (TextView) findViewById(R.id.textViewWarehouseID);
+        shipment_shipment_id = (TextView) findViewById(R.id.textViewShipmentID);
+        shipment_method = (TextView) findViewById(R.id.textViewShipmentMethod);
+        shipment_weight = (TextView) findViewById(R.id.textViewShipmentWeight);
+        shipment_receipt = (TextView) findViewById(R.id.textViewReceipt);
+        shipment_departure = (TextView) findViewById(R.id.textViewDeparture);
+
+        warehouse_id.setText(String.format("Warehouse ID: %s",application.getCurrentWarehouseID()));
+        warehouse_name.setText(String.format("Name: %s",company.getWarehouseName(application.getCurrentWarehouseID())));
+
         addShipmentButton.setOnClickListener(this);
         toggleActiveInactiveButton.setOnClickListener(this);
         toggleRecieptButton.setOnClickListener(this);
         editWarehouseButton.setOnClickListener(this);
 
+        refreshShipmentList();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
         refreshShipmentList();
     }
 
@@ -71,27 +95,31 @@ public class ViewWarehouseActivity extends AppCompatActivity implements View.OnC
         }
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
-    @Override
-    public void onResume(){
-        super.onResume();
-        refreshShipmentList();
-    }
-
     public void refreshShipmentList(){
         warehouse_contents = company.readWarehouseContent(application.getCurrentWarehouseID());
         String[] shipment_id_list = warehouse_contents.keySet().toArray(new String[0]);
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.shipment_list_view, shipment_id_list);
+        adapter = new ArrayAdapter<String>(this,R.layout.shipment_list_view, shipment_id_list);
         shipmentList = (ListView) findViewById(R.id.shipment_list_view);
         shipmentList.setAdapter(adapter);
+        shipmentList.setOnItemClickListener(this::onItemClick);
+    }
+
+    public void refreshShipmentInfo(String shipment_id){
+        Shipment current_shipment = warehouse_contents.get(shipment_id);
+        Date receipt = new Date(current_shipment.getReceipt_date());
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+        shipment_warehouse_id.setText(current_shipment.getWarehouse_id());
+        shipment_shipment_id.setText(current_shipment.getShipment_id());
+        shipment_method.setText(current_shipment.getShipment_method().toString());
+        shipment_weight.setText(String.format("%.2f lbs",current_shipment.getWeight()));
+        shipment_receipt.setText(formatter.format(receipt));
+        shipment_departure.setText("N/A");
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String current_shipment_id = parent.getItemAtPosition(position).toString();
+        refreshShipmentInfo(current_shipment_id);
     }
 }
