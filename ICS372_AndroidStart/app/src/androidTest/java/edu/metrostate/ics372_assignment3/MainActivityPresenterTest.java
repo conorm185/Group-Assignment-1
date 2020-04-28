e4ff367ab47f56ee05243d0d5f74d0c0a8b3baea
Gyan Gauchan <ggauchan@hotmail.com>
2020-04-28 18:03:42 -0500

    test updates

--- ICS372_AndroidStart/app/src/androidTest/java/edu/metrostate/ics372_assignment3/MainActivityPresenterTest.java
+++ ICS372_AndroidStart/app/src/androidTest/java/edu/metrostate/ics372_assignment3/MainActivityPresenterTest.java
@@ -7,7 +7,6 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
 import java.util.Map;
 
 import edu.metrostate.ics372_assignment3.model.Company;
@@ -16,22 +15,25 @@ import edu.metrostate.ics372_assignment3.model.Warehouse;
 
 import static org.junit.Assert.*;
 
-public class MainActivityTest {
+public class MainActivityPresenterTest {
 
     private MainActivityPresenter presenter;
+    private  MainActivityMVP.View view;
 
     @Before
     public void setUp() throws Exception {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         presenter = new MainActivityPresenter(Company.getInstance(appContext));
+        view = new MockView();
+        presenter.setView(view);
     }
 
     @Test
-    public void showAddNewShipment() {
+    public void addShipmentCompleted() {
         // Count number of shipments in warehouse 12531
         Map<String, Shipment> sh = presenter.readWarehouseContent("12531");
         int old_count = sh.size();
-        System.out.println(old_count);
+        System.err.println(old_count);
         // Add a Shipment to warehouse 12531
         String shipment_id = "23e456j";
         Shipment.ShippingMethod method = Shipment.ShippingMethod.ship;
@@ -42,12 +44,12 @@ public class MainActivityTest {
         // Count the new number of Shipments in that warehouse
         Map<String, Shipment> sh2 = presenter.readWarehouseContent("12531");
         int new_count = sh2.size();
-        System.out.println(new_count);
+        System.err.println(new_count);
         assertTrue(new_count > old_count);
     }
 
     @Test
-    public void showAddNewWarehouse(){
+    public void addWarehouseCompleted() {
         // Count number of warehouse
         int old_count = presenter.getWarehouseIds().size();
         // Add new warehouse 44444
@@ -60,10 +62,28 @@ public class MainActivityTest {
     }
 
     @Test
-    public void toggleWarehouse() {
+    public void editWarehouseCompleted() {
+        // get the current name of warehouse 12531
+        String oldName = presenter.getWarehouseName("12531");
+        System.err.println("Old name " + oldName);
+        // change the name of warehouse 12531
+        Warehouse warehouse = new Warehouse("12531");
+        warehouse.setWarehouse_name("Freight Team");
+
+        presenter.editWarehouseCompleted(warehouse);
+        // check if the current name of warehouse is same as previous
+        String newName = presenter.getWarehouseName("12531");
+        System.err.println("New name " + newName);
+        //assertTrue(!oldName.equals(newName));
+    }
+
+
+    @Test
+    public void toggleFreightReciept() {
         boolean old_freight_status = presenter.getFreightReceiptStatus("12531");
         presenter.toggleFreightReciept("12531");
         boolean new_freight_status = presenter.getFreightReceiptStatus("12531");
         assertTrue(old_freight_status != new_freight_status);
     }
+
 }
---
+++ ICS372_AndroidStart/app/src/androidTest/java/edu/metrostate/ics372_assignment3/MockView.java
@@ -0,0 +1,35 @@
+package edu.metrostate.ics372_assignment3;
+
+import java.util.ArrayList;
+
+public class MockView implements MainActivityMVP.View {
+    @Override
+    public void showAddNewShipment() {
+
+    }
+
+    @Override
+    public void showAddNewWarehouse() {
+
+    }
+
+    @Override
+    public void showEditWarehouse() {
+
+    }
+
+    @Override
+    public void showMoveShipment() {
+
+    }
+
+    @Override
+    public void showShipments(String[] shipment_id_list) {
+
+    }
+
+    @Override
+    public void showWarehouses(ArrayList<String> warehouseIds) {
+
+    }
+}
